937b70a8aae1cb1d0ba596205e3fcc4ef0bed9d9
willowv <eps.willow@gmail.com>
2026-08-06 20:32:37 -0700

    Give the player a craving for an uneaten food when they wake up from sleep.

--- src/client/java/dev/willowv/solcc/client/SpiceOfLifeCarrotCakeClient.java
+++ src/client/java/dev/willowv/solcc/client/SpiceOfLifeCarrotCakeClient.java
@@ -1,17 +1,25 @@
 package dev.willowv.solcc.client;
 
+import dev.willowv.solcc.SpiceOfLifeCarrotCake;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import dev.willowv.solcc.ClientboundUpdateFoodHistoryPacket;
 import dev.willowv.solcc.IFoodHistoryManager;
 
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import static dev.willowv.solcc.SpiceOfLifeCarrotCake.*;
 
@@ -24,6 +32,22 @@ public class SpiceOfLifeCarrotCakeClient implements ClientModInitializer {
 		LOGGER.debug("[Client] Initialized.");
 		ItemTooltipCallback.EVENT.register(SpiceOfLifeCarrotCakeClient::itemTooltipCallback);
 		ClientPlayNetworking.registerGlobalReceiver(ClientboundUpdateFoodHistoryPacket.TYPE, new ClientboundUpdateFoodHistoryPacketHandler());
+
+		EntitySleepEvents.STOP_SLEEPING.register(SpiceOfLifeCarrotCakeClient::wakeUpCraving);
+	}
+
+	// Implements Functional Interface EntitySleepEvents.StopSleeping
+	private static void wakeUpCraving(LivingEntity entity, BlockPos sleepingPos) {
+		if(entity instanceof AbstractClientPlayer player) {
+			Set<Item> uniqueFoodsEaten = ((IFoodHistoryManager) player).solcc$getUniqueFoodsEaten();
+			Set<Item> uneatenUniqueFoods = SpiceOfLifeCarrotCake.getUneatenFoods(uniqueFoodsEaten);
+            uneatenUniqueFoods.stream()
+                    .skip(new Random().nextInt((uneatenUniqueFoods.size())))
+                    .findFirst()
+					.ifPresent(food -> player.displayClientMessage(
+                            Component.translatable("player.solcc.craving", food.getDefaultInstance().getDisplayName())
+                                    .withStyle(ChatFormatting.DARK_AQUA, ChatFormatting.ITALIC), false));
+        }
 	}
 
 	private static void itemTooltipCallback(ItemStack stack, TooltipFlag flag, List<Component> lines) {
